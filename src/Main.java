7227df7d61fd687044229ef9104046305c4b2c51
mol1xx <139921089+mol1xx@users.noreply.github.com>
2023-07-22 02:00:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,197 +1,139 @@
 import java.util.Scanner;
+import java.io.IOException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
         Scanner scaner = new Scanner(System.in);
         String str = scaner.nextLine();
-        str = calc(str);
+        try {
+            str = calc(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(str);
     }
 
-    public static String calc(String input) {
+    public static String calc(String input) throws IOException {
         int number1, number2, solution, romeNumber1, romeNumber2;
         romeNumber1 = 0;
         romeNumber2 = 0;
-        String array[] = input.split(" ");
+        String[] array = input.split(" ");
         if (array.length != 3) {
-            input = "throws Exception";
-            return input;
+            throw new IOException();
         }
         switch (array[0]) {
-            case "1":
-                number1 = 1;
-                break;
-            case "2":
-                number1 = 2;
-                break;
-            case "3":
-                number1 = 3;
-                break;
-            case "4":
-                number1 = 4;
-                break;
-            case "5":
-                number1 = 5;
-                break;
-            case "6":
-                number1 = 6;
-                break;
-            case "7":
-                number1 = 7;
-                break;
-            case "8":
-                number1 = 8;
-                break;
-            case "9":
-                number1 = 9;
-                break;
-            case "10":
-                number1 = 10;
-                break;
-            case "I":
+            case "1" -> number1 = 1;
+            case "2" -> number1 = 2;
+            case "3" -> number1 = 3;
+            case "4" -> number1 = 4;
+            case "5" -> number1 = 5;
+            case "6" -> number1 = 6;
+            case "7" -> number1 = 7;
+            case "8" -> number1 = 8;
+            case "9" -> number1 = 9;
+            case "10" -> number1 = 10;
+            case "I" -> {
                 number1 = 1;
                 romeNumber1 = 1;
-                break;
-            case "II":
-                number1 = 2;
-                romeNumber1 = 1;
-                break;
-            case "III":
-                number1 = 3;
-                romeNumber1 = 1;
-                break;
-            case "IV":
-                number1 = 4;
-                romeNumber1 = 1;
-                break;
-            case "V":
-                number1 = 5;
-                romeNumber1 = 1;
-                break;
-            case "VI":
-                number1 = 6;
-                romeNumber1 = 1;
-                break;
-            case "VII":
-                number1 = 7;
-                romeNumber1 = 1;
-                break;
-            case "VIII":
-                number1 = 8;
-                romeNumber1 = 1;
-                break;
-            case "IX":
-                number1 = 9;
-                romeNumber1 = 1;
-                break;
-            case "X":
-                number1 = 10;
-                romeNumber1 = 1;
-                break;
-            default: {
-                input = "throws Exception";
-                return input;
             }
+            case "II" -> {
+                number1 = 2;
+                romeNumber1 = 1;
+            }
+            case "III" -> {
+                number1 = 3;
+                romeNumber1 = 1;
+            }
+            case "IV" -> {
+                number1 = 4;
+                romeNumber1 = 1;
+            }
+            case "V" -> {
+                number1 = 5;
+                romeNumber1 = 1;
+            }
+            case "VI" -> {
+                number1 = 6;
+                romeNumber1 = 1;
+            }
+            case "VII" -> {
+                number1 = 7;
+                romeNumber1 = 1;
+            }
+            case "VIII" -> {
+                number1 = 8;
+                romeNumber1 = 1;
+            }
+            case "IX" -> {
+                number1 = 9;
+                romeNumber1 = 1;
+            }
+            case "X" -> {
+                number1 = 10;
+                romeNumber1 = 1;
+            }
+            default -> throw new IOException();
         }
         switch (array[2]) {
-            case "1":
-                number2 = 1;
-                break;
-            case "2":
-                number2 = 2;
-                break;
-            case "3":
-                number2 = 3;
-                break;
-            case "4":
-                number2 = 4;
-                break;
-            case "5":
-                number2 = 5;
-                break;
-            case "6":
-                number2 = 6;
-                break;
-            case "7":
-                number2 = 7;
-                break;
-            case "8":
-                number2 = 8;
-                break;
-            case "9":
-                number2 = 9;
-                break;
-            case "10":
-                number2 = 10;
-                break;
-            case "I":
+            case "1" -> number2 = 1;
+            case "2" -> number2 = 2;
+            case "3" -> number2 = 3;
+            case "4" -> number2 = 4;
+            case "5" -> number2 = 5;
+            case "6" -> number2 = 6;
+            case "7" -> number2 = 7;
+            case "8" -> number2 = 8;
+            case "9" -> number2 = 9;
+            case "10" -> number2 = 10;
+            case "I" -> {
                 number2 = 1;
                 romeNumber2 = 1;
-                break;
-            case "II":
-                number2 = 2;
-                romeNumber2 = 1;
-                break;
-            case "III":
-                number2 = 3;
-                romeNumber2 = 1;
-                break;
-            case "IV":
-                number2 = 4;
-                romeNumber2 = 1;
-                break;
-            case "V":
-                number2 = 5;
-                romeNumber2 = 1;
-                break;
-            case "VI":
-                number2 = 6;
-                romeNumber2 = 1;
-                break;
-            case "VII":
-                number2 = 7;
-                romeNumber2 = 1;
-                break;
-            case "VIII":
-                number2 = 8;
-                romeNumber2 = 1;
-                break;
-            case "IX":
-                number2 = 9;
-                romeNumber2 = 1;
-                break;
-            case "X":
-                number2 = 10;
-                romeNumber2 = 1;
-                break;
-            default: {
-                input = "throws Exception";
-                return input;
             }
+            case "II" -> {
+                number2 = 2;
+                romeNumber2 = 1;
+            }
+            case "III" -> {
+                number2 = 3;
+                romeNumber2 = 1;
+            }
+            case "IV" -> {
+                number2 = 4;
+                romeNumber2 = 1;
+            }
+            case "V" -> {
+                number2 = 5;
+                romeNumber2 = 1;
+            }
+            case "VI" -> {
+                number2 = 6;
+                romeNumber2 = 1;
+            }
+            case "VII" -> {
+                number2 = 7;
+                romeNumber2 = 1;
+            }
+            case "VIII" -> {
+                number2 = 8;
+                romeNumber2 = 1;
+            }
+            case "IX" -> {
+                number2 = 9;
+                romeNumber2 = 1;
+            }
+            case "X" -> {
+                number2 = 10;
+                romeNumber2 = 1;
+            }
+            default -> throw new IOException();
         }
         switch (array[1]) {
-            case "+": {
-                solution = number1 + number2;
-                break;
-            }
-            case "-": {
-                solution = number1 - number2;
-                break;
-            }
-            case "*": {
-                solution = number1 * number2;
-                break;
-            }
-            case "/": {
-                solution = number1 / number2;
-                break;
-            }
-            default: {
-                input = "throws Exception";
-                return input;
-            }
+            case "+" -> solution = number1 + number2;
+            case "-" -> solution = number1 - number2;
+            case "*" -> solution = number1 * number2;
+            case "/" -> solution = number1 / number2;
+            default -> throw new IOException();
         }
         if (romeNumber1 == 0 && romeNumber2 == 0) {
             input = Integer.toString(solution);
@@ -210,52 +152,28 @@ public class Main {
                 solution = solution - 50;
                 input = input + "L";
             }
+            if (solution >= 40) {
+                solution = solution - 40;
+                input = input + "XL";
+            }
             while (solution>=10) {
                 solution = solution -10;
                 input = input + "X";
             }
             switch (solution) {
-                case 9: {
-                    input = input + "IX";
-                    break;
-                }
-                case 8: {
-                    input = input + "VIII";
-                    break;
-                }
-                case 7: {
-                    input = input + "VII";
-                    break;
-                }
-                case 6: {
-                    input = input + "VI";
-                    break;
-                }
-                case 5: {
-                    input = input + "V";
-                    break;
-                }
-                case 4: {
-                    input = input + "IV";
-                    break;
-                }
-                case 3: {
-                    input = input + "III";
-                    break;
-                }
-                case 2: {
-                    input = input + "II";
-                    break;
-                }
-                case 1: {
-                    input = input + "I";
-                    break;
-                }
+                case 9 ->  input = input + "IX";
+                case 8 -> input = input + "VIII";
+                case 7 -> input = input + "VII";
+                case 6 -> input = input + "VI";
+                case 5 -> input = input + "V";
+                case 4 -> input = input + "IV";
+                case 3 -> input = input + "III";
+                case 2 -> input = input + "II";
+                case 1 -> input = input + "I";
             }
         }
         else {
-            input = "throws Exception";
-            return input;
+            throw new IOException();
         }
         return input;
     }
